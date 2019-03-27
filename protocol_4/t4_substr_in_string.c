648cf19386605e80ea7a172036507dee5f0956a4
Martin Kontilov <m.kontilov@gmail.com>
2019-03-27 22:01:40 +0200

    Implemented all substrings in a string

---
+++ protocol_4/t4_substr_in_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int all_substrings (char* str, int str_len, char* substr, int substr_len, int* positions) {
+    int i = 0;
+    int position_index = 0;
+    int occurrences = 0;
+    char* pos = str;
+
+    while (i < str_len) {
+        pos = strstr(pos + 1, substr);
+        if (pos != NULL) {
+            i = pos - str;
+            positions[position_index++] = i;
+            occurrences++;
+        } else {
+            break;
+        }
+    }
+
+    return occurrences;
+}
+
+int main () {
+    char* str = "This is a test, test";
+    char* substr = "test";
+    int substr_len = (int)strlen(substr);
+
+    printf("String length: %d \n", strlen(str));
+    int positions_length = strlen(str) + 1; 
+    int* positions = malloc( positions_length );
+    int occurrences = all_substrings(str, strlen(str), substr, strlen(substr) + 1, positions);
+
+    printf("Occurrences %d \n", occurrences);
+    for (int i = 0; i < occurrences; i++) {
+        printf("Substring at position %d \n", positions[i]);
+    }
+}
