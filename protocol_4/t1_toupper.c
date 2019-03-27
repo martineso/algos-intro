607b7555e7a0682af0dedae484118e9367dfed06
Martin Kontilov <m.kontilov@gmail.com>
2019-03-27 20:32:41 +0200

    Implementing toupper, atoi and itoa tasks

--- protocol_4/t1_toupper.c
+++ protocol_4/t1_toupper.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-#include <ctype.h>
+
+char to_upper(char c) 
+{
+  if (c == 'a' && c <= 'Z') 
+  {
+     return c + 'A' - 'a';
+  }
+  return c;
+}
 
 int main()
 {
    char c;
    while ((c = getc(stdin)) != EOF)
    {
-      putchar(toupper(c));
+      putchar(to_upper(c));
    }
 
    return 0;
---
+++ protocol_4/t2_str_to_int.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int custom_atoi (const char *str) 
+{
+    int result = 0;
+    int i = 0;
+
+    while (str[i] != '\0') 
+    {
+        
+        if (str[i] == ' ')
+        {
+            i++;
+            continue;
+        }
+
+        result = (result * 10) + str[i] - '0';
+        i++;
+    }
+
+    return result;
+}
+
+int main () 
+{
+    unsigned int i;
+    char *s = " 9885";
+    i = custom_atoi(s);
+    printf("%d\n", i);
+    return 0;
+}
