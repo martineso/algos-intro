ebfba21fe11a7aa9fd99b2f6f2b9abc84431090e
Martin Kontilov <m.kontilov@gmail.com>
2019-03-31 16:39:17 +0300

    Fixed merge_arr

--- protocol_5/t6_merge_arr.c
+++ protocol_5/t6_merge_arr.c
@@ -33,7 +33,7 @@ int* merge_arr (int* a, int a_len, int* b, int b_len) {
 
 int main () {
 
-    int a[] = {1, 3, 5, 7, 9, 11, 13, 14};
+    int a[] = {1, 3, 5, 7, 9, 11, 13, 15};
     int a_len = 8;
     int b[] = {2, 4, 6, 8, 10, 12};
     int b_len = 6;
